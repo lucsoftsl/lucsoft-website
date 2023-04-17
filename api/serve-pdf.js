7f2207b1928ea048fc56d808207ff6327d57cec5
Alexandra Mile <alexandramile@Alexandras-MacBook-Pro.local>
2023-04-17 11:40:37 +0200

    add

---
+++ api/serve-pdf.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (req, res) => {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Specify the PDF file path
+  const pdfFilePath = path.join(__dirname, '../public/output1.pdf');
+
+  // Serve the PDF file
+  fs.readFile(pdfFilePath, (err, data) => {
+    if (err) {
+      res.status(500).json({ error: 'Failed to read the PDF file.' });
+      return;
+    }
+
+    res.setHeader('Content-Type', 'application/pdf');
+    res.status(200).send(data);
+  });
+};
